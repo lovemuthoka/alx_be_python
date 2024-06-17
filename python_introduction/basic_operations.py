8f8987366a50e8232f0e06cc397db99c875eafa8
lovemuthoka <lovemuthoka3@gmail.com>
2024-06-17 07:34:33 -0700

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+# Define variables
+number1 = 10
+number2 = 5
+
+# Calculate sum
+sum_result = number1 + number2
+print(f"Sum of {number1} and {number2} is {sum_result}")
+
+# Calculate difference
+difference_result = number1 - number2
+print(f"Difference of {number1} and {number2} is {difference_result}")
+
+# Calculate product
+product_result = number1 * number2
+print(f"Product of {number1} and {number2} is {product_result}")
+
