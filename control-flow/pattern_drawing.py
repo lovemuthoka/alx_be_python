4af7b401853e9cd33bf73213a30c7514586ed26a
lovemuthoka <lovemuthoka3@gmail.com>
2024-06-28 07:05:52 -0700

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,13 +1,20 @@
 # pattern_drawing.py
 
+def draw_square_pattern(size):
+    row = 0
+    while row < size:
+        for _ in range(size):
+            print("*", end="")
+        print()
+        row += 1
+
 def main():
     try:
         size = int(input("Enter the size of the pattern: "))
-        if size > 0:
-            for _ in range(size):
-                print("*" * size)
-        else:
+        if size <= 0:
             print("Please enter a positive integer.")
+        else:
+            draw_square_pattern(size)
     except ValueError:
         print("Invalid input. Please enter a positive integer.")
 
