8dc845e5dd2d87fe5f9960fcd23e197f2c36d332
lovemuthoka <lovemuthoka3@gmail.com>
2024-07-04 15:40:17 -0700

    operation

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,25 @@
-from arithmetic_operations import perform_operation
+def perform_operation(num1, num2, operation):
+    if operation =='add':
+        return num1 + num2
+    
+    elif operation == 'subtract':
+        return num1 - num2
+    
+    elif operation =='multiply':
+        return num1 * num2 
+    
+    elif operation == 'divide':
+        if num2 == 0:
+            return enter a valid number please
+        else:
+            return num1 / num2
+       
+    
+    else:
+       print("Invalid number and operation:")
 
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+num1 = float(input("Enter number of choice:"))
+num2 = float(input("Enter number of choice:"))
+operation =input("Enter operation operator of choice:")
 
-    result = perform_operation(num1, num2, operation)
-    print(f"Result: {result}")
-
-if __name__ == "__main__":
-    main()
+perform_operation(num1, num2, operation)
