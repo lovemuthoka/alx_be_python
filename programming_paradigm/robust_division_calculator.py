c7cd5a7630f04c8ba96ccf38e43b4abada587fd3
lovemuthoka <lovemuthoka3@gmail.com>
2024-07-12 06:08:34 -0700

    division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,32 +1,14 @@
 def safe_divide(numerator, denominator):
     try:
-        numerator = float(numerator)
-        denominator = float(denominator)
-    except ValueError:
-        return "Error: Non-numeric input. Please enter valid numbers."
+        num = float(numerator)
+        denom= float(denominator)
 
-    try:
-        if denominator == 0:
-            return "Error: Division by zero is not allowed."
-        else:
-            return numerator / denominator
+        result = num / denom
     except ZeroDivisionError:
-        return "Error: Division by zero is not allowed."
+        return "Error: Cannot divide by zero."
+    except ValueError:
+        return "Error: Please enter numeric values only."
+    except Exception as e:
+        return f"Unexpected error occured: {str(e)}"
+    return f"The result of the division is {result:.1f}"
 
-    import sys
-from robust_division_calculator import safe_divide
-
-def main():
-    if len(sys.argv)!= 3:
-        print("Usage: python main.py <numerator> <denominator>")
-        return
-
-    numerator = sys.argv[1]
-    denominator = sys.argv[2]
-
-    result = safe_divide(numerator, denominator)
-
-    print(result)
-
-if __name__ == "__main__":
-    main()
